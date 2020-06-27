27f870c253d35c02cd3abc70fe16ba71479bb772
JackRazors <38985846+JackRazors@users.noreply.github.com>
2020-06-27 15:06:42 -0400

    Add files via upload
    - Changed argparse to group -fp and -fl (filepath/filelist)
    - Added the ability to load from a file list (.txt file - one entry per line)
    - You can not use SCANUSER (--username), SCANPASS (--password) and SCANURL (--url) environment variables
      rather than having to specify them each time!
    - ConnectionTimeout error checking so the entire script doesn't barf when it occurs
    - Ability to specify an archive password.
    - If archive is scanned, report on all files scanned within archive and not just
      the scan result of the archive (--archivepwd)

--- scan.py
+++ scan.py
@@ -1,14 +1,17 @@
 # OPSWAT Metascan scanner
 # Author: Ian Redden <ian.redden@rsa.com>
-# 6/23/2020 rev. 5
+# 6/27/2020 rev. 6
 #
 # Change log:
-# - added glob for unix-style wildcards in file paths (required the --recursive flag set)
-# - added csv output (defaults to output.csv, filename set with -o/--output)
-# - added output to stdout (requires the --output flag NOT set)
-# - added json output (using prettyprinter)
+# - Changed argparse to group -fp and -fl (filepath/filelist)
+# - Added the ability to load from a file list (.txt file - one entry per line)
+# - You can not use SCANUSER (--username), SCANPASS (--password) and SCANURL (--url) environment variables
+#   rather than having to specify them each time!
+# - ConnectionTimeout error checking so the entire script doesn't barf when it occurs
+# - Ability to specify an archive password.
+# - If archive is scanned, report on all files scanned within archive and not just
+#   the scan result of the archive (--archivepwd)
 #
-import pprint
 import requests
 import json
 import os
@@ -16,6 +19,22 @@ import argparse
 import csv
 import glob
 from time import sleep
+from enum import Enum
+from requests import exceptions
+
+
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
 
 #
 # Setup Argument Parsing using argparse
@@ -28,11 +47,17 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
     epilog=help_str)
 
-parser.add_argument('-fp', '--filepath', help='File or directory to scan', required=True)
-parser.add_argument('--url', required=True, help='Hostname/URL of Metascan server. (i.e. https://localhost:8008)')
-parser.add_argument('-u', '--username', required=True, help='Username for your Metascan server (i.e. admin)')
-parser.add_argument('-p', '--password', required=True, help='Password for your Metascan server.')
+# either/or -- either specify a filepath or filelist
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-fp', '--filepath', help='File or directory to scan')
+group.add_argument('-fl', '--filelist', help='Path to text file containing file listing to scan')
+
+#
+parser.add_argument('--url', required=True, action=EnvDefault, envvar='SCANURL', help='Hostname/URL of Metascan server. (i.e. https://localhost:8008)')
+parser.add_argument('-u', '--username', required=True, action=EnvDefault, envvar='SCANUSER', help='Username for your Metascan server (i.e. admin)')
+parser.add_argument('-p', '--password', required=True, action=EnvDefault, envvar='SCANPASS', help='Password for your Metascan server.')
 parser.add_argument('-o', '--output', required=False, help='Filename to store results in CSV format.')
+parser.add_argument('-ap', '--archivepwd', required=False, help='Password for encrypted/compressed archives')
 parser.add_argument('-j', '--json', action='store_true', required=False, help='JSON output')
 parser.add_argument('-r', '--recursive', action='store_true', required=False,
                     help='Recursively traverse paths with wildcards.')
@@ -40,6 +65,13 @@ args = parser.parse_args()
 
 
 class MetaScanner:
+    class ProcessResultsTable(Enum):
+        CLEAN = 0
+        INFECTED = 1
+
+        def __str__(self):
+            return self.name
+
     def __init__(self, url, username, password):
         self.session_id = ''
         self.username = username
@@ -77,9 +109,9 @@ class MetaScanner:
             sleep(2)
             return self.get_sample(data_id)
 
-    def scan(self, filepath, recursive=False):
+    def scan(self, files, archivepwd=None):
         url = '{0}/file'.format(self.url)
-        files = self.get_files(filepath, recursive)
+
         for file in files:
             print("Submitting file %s ..." % file)
             try:
@@ -87,30 +119,32 @@ class MetaScanner:
                     file_data = f.read()
 
                     filename = os.path.basename(file)  # submit the base filename (otherwise its blank in UI)
-                    headers = dict(apikey=self.session_id, filename=filename)
+                    headers = dict(apikey=self.session_id, filename=filename, archivepwd=archivepwd)
 
                     r = requests.post(url=url, headers=headers, data=file_data)
                     json_r = json.loads(r.text)
 
                     if "data_id" in json_r:  # no data_id in response = something bad happened
                         data_id = json.loads(r.text)["data_id"]
-                        scan_results = self.get_sample(data_id)
-                        self.db[data_id] = scan_results
+                        self.get_report(data_id)
                     else:
                         raise SystemError(json_r["err"])
-
-            except PermissionError:
-                print("Could not open file.")
-            except SystemError:
-                print("An internal error occured.  Unable to scan file.")
+            except:
+                print("Scan error occured. Skipping ...")
 
     def csv(self, report_filename):  # format results in csv format
         rows = []
         for data_id, record in self.db.items():
             scan_time_ms = record["scan_results"]["total_time"]
 
+            # check for archive
             file_info = record["file_info"]
-            display_name = file_info["display_name"]
+            if "parent_path" in record:
+                file_name = record["file_info"]["display_name"]
+                display_name = "{0}:{1}".format(record["parent_path"][-1]["display_name"], file_name)
+            else:
+                display_name = record["file_info"]["display_name"]
+
             file_size = file_info["file_size"]
             file_type = file_info["file_type"]
             file_type_description = file_info["file_type_description"]
@@ -133,15 +167,15 @@ class MetaScanner:
                 def_time = scan_detail["def_time"]
                 eng_id = scan_detail["eng_id"]
                 location = scan_detail["location"]
-                scan_result_i = scan_detail["scan_result_i"]
+                scan_result_i = self.ProcessResultsTable(scan_detail["scan_result_i"])
                 scan_time = scan_detail["scan_time"]
                 threat_found = scan_detail["threat_found"]
                 wait_time = scan_detail["wait_time"]
 
-                if scan_result_i == 0:
-                    scan_result = "OK"
-                else:
+                if scan_result_i == self.ProcessResultsTable.INFECTED:
                     scan_result = threat_found
+                else:
+                    scan_result = "OK"
 
                 row_data[engine] = scan_result
             rows.append(row_data)
@@ -169,15 +203,32 @@ class MetaScanner:
                 self.csv(report_filename)
         else:
             for data_id, record in self.db.items():
-                display_name = record["file_info"]["display_name"]
+                if "parent_path" in record:
+                    file_name = record["file_info"]["display_name"]
+                    display_name = "{0}:{1}".format(record["parent_path"][-1]["display_name"], file_name)
+                else:
+                    display_name = record["file_info"]["display_name"]
+
                 sha1 = record["file_info"]["sha1"]
                 scan_result = record["scan_results"]["scan_all_result_a"]
                 file_type = record["file_info"]["file_type_description"]
 
                 print("{0} ({1}) => {2} - SHA1: {3}".format(display_name, file_type, scan_result, sha1))
 
-    @staticmethod
-    def get_files(filepath, recursive=False):  # use glob for wildcards if recursive is set
+    def get_report(self, data_id):
+        scan_results = self.get_sample(data_id)
+        if "extracted_files" in scan_results:
+            extracted_files = scan_results["extracted_files"]
+            files_in_archive = extracted_files["files_in_archive"]
+            for archive in files_in_archive:
+                id = archive["data_id"]
+                print("(Archive) Retrieving report {0} ...".format(id))
+                db_entry = self.get_sample(id)
+                self.db[id] = db_entry
+        else:
+            self.db[data_id] = scan_results
+
+    def get_files(self, filepath, recursive=False):  # use glob for wildcards if recursive is set
         files = []
         if os.path.isdir(filepath):
             if recursive:
@@ -203,5 +254,19 @@ class MetaScanner:
 
 scan_engine = MetaScanner(args.url, args.username, args.password)
 scan_engine.login()
-scan_engine.scan(args.filepath, args.recursive)
+
+if args.filepath:
+    files = scan_engine.get_files(args.filepath, args.recursive)
+elif args.filelist:
+    # open the file list
+    files = []
+    with open(args.filelist, 'r') as file_listing:
+        for row in file_listing:
+            file = row.rstrip('\n')
+            if not os.path.isdir(file):
+                files.append(file)
+            else:
+                print("Skipping directory {0} ...".format(file))
+
+scan_engine.scan(files, archivepwd=args.archivepwd)
 scan_engine.results(args.output, args.json)
